3e03dfba4411ecdec0483edbb97d806376e39448
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2018-12-29 14:05:18 +0900

    Correct function name

--- functions/__ffg_checkout.fish
+++ functions/__ffg_checkout.fish
@@ -1,3 +1,3 @@
-function __fzf_git_checkout -d "Fuzzy-find and checkout a branch"
+function __ffg_checkout -d "Fuzzy-find and checkout a branch"
   git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
 end
